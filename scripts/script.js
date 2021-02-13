87735753869d8ca0162ff256ab4f7b454b8d0c78
Kenneth Shin <60720965+kbshin01@users.noreply.github.com>
2021-02-13 15:19:04 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  if (myStorage.getItem('products') == null) {
+  if (localStorage.getItem('products') == null) {
     fetch('https://fakestoreapi.com/products')
       .then(response => response.json())
       .then(data => {
