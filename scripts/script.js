3d5f60510e26e5044f8433900cb709f2a13d307b
Kenneth Shin <60720965+kbshin01@users.noreply.github.com>
2021-02-13 15:46:11 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -6,15 +6,14 @@ window.addEventListener('DOMContentLoaded', () => {
       .then(response => response.json())
       .then(data => {
         localStorage.setItem('products', JSON.stringify(data));
+        for (let i = 0; i < data.length; i++) {
+          let productItem = document.createElement('product-item');
+          productItem.setAttribute('id', data[i].getAttribute('id'));
+          productItem.setAttribute('image', data[i].getAttribute('image'));
+          productItem.setAttribute('title', data[i].getAttribute('title'));
+          productItem.setAttribute('price', data[i].getAttribute('price'));
+          (document.getElementById('product-list')).appendChild(productItem);
+        }
       });
-  }
-  
-  for (let i = 0; i < data.length; i++) {
-    let productItem = document.createElement('product-item');
-    productItem.setAttribute('id', data[i].getAttribute('id'));
-    productItem.setAttribute('image', data[i].getAttribute('image'));
-    productItem.setAttribute('title', data[i].getAttribute('title'));
-    productItem.setAttribute('price', data[i].getAttribute('price'));
-    (document.getElementById('product-list')).appendChild(productItem);
   }
 });
