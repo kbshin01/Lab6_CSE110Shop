bd4d548ea47eec694af869f1543c2c0841dbcb8f
Kenneth Shin <60720965+kbshin01@users.noreply.github.com>
2021-02-13 14:45:03 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,21 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  if(myStorage.getItem('products') == null) {
+  if (myStorage.getItem('products') == null) {
     fetch('https://fakestoreapi.com/products')
       .then(response => response.json())
       .then(data => {
         localStorage.setItem('products', JSON.stringify(data));
-      })
+      });
+  }
+  
+  let products = JSON.parse(localStorage.getItem('products'));
+  for (let i = 0; i < products.length; i++) {
+    let productItem = document.createElement('product-item');
+    productItem.setAttribute('id', products[i].getAttribute('id'));
+    productItem.setAttribute('image', products[i].getAttribute('image'));
+    productItem.setAttribute('title', products[i].getAttribute('title'));
+    productItem.setAttribute('price', products[i].getAttribute('price'));
+    document.getElementById('product-list').appendChild(productItem);
   }
 });
